fb01ee7e2982e4569c9c2337b5da790af140214f
PranithVP <pranithvrp@gmail.com>
2026-01-08 20:17:33 -0500

    longest consecutive sequence

--- Medium/128_longest_consecutive_sequence.py
+++ Medium/128_longest_consecutive_sequence.py
@@ -2,18 +2,15 @@ from typing import List
 
 def longestConsecutive(nums: List[int]) -> int:
     elems = set(nums)
-    if len(nums) == 0: return 0
-    minimum = min(nums)
-    maximum = max(nums)
-
+    
     max_seq = 0
-    curr = 0
-    for i in range(minimum, maximum+1):
-        if i in elems:
-            curr += 1
-            if curr > max_seq:
-                max_seq = curr
-        else:
-            curr = 0
+    for elem in elems:
+        curr = 0
+        if elem - 1 not in elems:
+            while elem in elems:
+                curr += 1
+                elem += 1
+        if curr > max_seq:
+            max_seq = curr
     
     return max_seq
