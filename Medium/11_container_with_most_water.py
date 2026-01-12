8d0326646106ef030fbe6bd172480711e3775623
PranithVP <pranithvrp@gmail.com>
2026-01-12 17:42:04 -0500

    container with most water

---
+++ Medium/11_container_with_most_water.py
@@ -0,0 +1,14 @@
+def maxArea(self, height: List[int]) -> int:
+    curr_max = 0
+
+    i = 0
+    j = len(height) - 1
+
+    while i < j:
+        curr = min(height[i], height[j])*(j-i)
+        max(curr_max = curr)
+        if height[i] < height[j]: i += 1
+        elif height[j] < height[i]: j-= 1
+        else: i += 1
+    
+    return curr_max
