6528c0e68b3e8b21c67cf573b552bcdcc149c9a4
PranithVP <pranithvrp@gmail.com>
2026-02-24 18:00:00 +0300

    n-ary preorder traversal

---
+++ Easy/589_nary_preorder_traversal.py
@@ -0,0 +1,17 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        res = []
+        if root:
+            res.append(root.val)
+            for ch in root.children:
+                res += self.preorder(ch)
+        return res
+        
