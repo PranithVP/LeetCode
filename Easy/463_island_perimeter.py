d245058326a5f82a5b75764414bea9d59bdea8a8
Pranith <110805848+PranithVP@users.noreply.github.com>
2026-07-02 01:18:04 -0400

    island perimeter

---
+++ Easy/463_island_perimeter.py
@@ -0,0 +1,28 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        visited = set()
+        perimeter = 0
+        
+        def dfs(i, j):
+            if i < 0 or j < 0 or i >= m or j >= n:
+                return 1
+            
+            if grid[i][j] == 0:
+                return 1
+                
+            if (i, j) in visited:
+                return 0
+                
+            visited.add((i, j))
+                
+            return dfs(i+1, j) + dfs(i-1, j) + dfs(i, j+1) + dfs(i, j-1)
+            
+        
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    perimeter += dfs(i, j)
+        
+        return perimeter
