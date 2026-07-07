4c424470b5d83adffb75e6de39360a127b532607
PranithVP <pranithvrp@gmail.com>
2026-07-07 19:58:43 -0400

    gas station

---
+++ Medium/134_gas_station.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        n = len(gas)
+        tank = 0
+        start = 0
+
+        if sum(gas) < sum(cost):
+            return -1
+
+        for i in range(n):
+            tank += gas[i] - cost[i]
+
+            if tank < 0:
+                tank = 0
+                start = i + 1
+            
+        return start
