cff3dbfb1dcc9ca67405b78f8ce3fdcd0aa8f298
PranithVP <pranithvrp@gmail.com>
2026-07-15 21:45:48 -0400

    median of two sorted arrays

--- Hard/4_median_of_two_sorted_arrays.py
+++ Hard/4_median_of_two_sorted_arrays.py
@@ -1,32 +1,27 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        if len(nums1) > len(nums2):
-            nums1, nums2 = nums2, nums1
-        
-        a, b = 0, len(nums1)
-        total = len(nums1) + len(nums2)
-        half = (len(nums1) + len(nums2) + 1) // 2
-        
+        A, B = nums1, nums2
+
+        if len(A) > len(B):
+            A, B = B, A
+
+        a, b = -1, len(A) - 1
+        half = (len(A) + len(B)) // 2
+
         while a <= b:
             i = (a + b) // 2
-            j = half - i
+            j = half - (i + 1) - 1
 
-            left1 = nums1[i-1] if i > 0 else float('-inf')
-            right1 = nums1[i] if i < len(nums1) else float('inf')
-            left2 = nums2[j-1] if j > 0 else float('-inf')
-            right2 = nums2[j] if j < len(nums2) else float('inf')
+            left1 = A[i] if i >= 0 else float('-inf')
+            right1 = A[i+1] if i+1 < len(A) else float('inf')
+            left2 = B[j] if j >= 0 else float('-inf')
+            right2 = B[j+1] if j+1 < len(B) else float('inf')
 
             if left1 <= right2 and left2 <= right1:
-                if total % 2 == 1:
-                    return max(left1, left2)
-                else:
-                    return (max(left1, left2) + min(right1, right2)) / 2
+                if (len(A) + len(B)) % 2 == 1:
+                    return min(right1, right2)
+                return (max(left1, left2) + min(right1, right2)) / 2
             elif left1 > right2:
                 b = i - 1
-            else:
+            elif left2 > right1:
                 a = i + 1
-        
-        if total % 2 == 1:
-            return max(left1, left2)
-        else:
-            return (max(left1, left2) + min(right1, right2)) / 2
