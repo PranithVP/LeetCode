b43d4ac0037c224a9e8cbdcad8105be81ccddd28
PranithVP <pranithvrp@gmail.com>
2026-08-07 11:02:13 -0400

    sort array by parity

---
+++ Easy/905_sort_array_by_parity.py
@@ -0,0 +1,13 @@
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        j = len(nums)-1
+        i = 0
+
+        while i < j:
+            if nums[i] % 2 == 1:
+                nums[i], nums[j] = nums[j], nums[i]
+                j -= 1
+            else:
+                i += 1
+        
+        return nums
