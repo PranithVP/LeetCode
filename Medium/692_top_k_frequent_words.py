bceae1cfcbfffc3c65a4f3c7ce1ec227b7f230df
PranithVP <pranithvrp@gmail.com>
2026-08-06 22:36:19 -0400

    top k frequent words

---
+++ Medium/692_top_k_frequent_words.py
@@ -0,0 +1,7 @@
+import heapq
+from collections import Counter
+
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        freq = Counter(words)
+        return [b for a, b in sorted([(b, a) for a, b in freq.items()], key=lambda x:(-x[0], x[1]))][:k]
