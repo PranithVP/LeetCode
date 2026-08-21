aee22cfc62a46ceeb6b6903492ffc9c11339f649
PranithVP <pranithvrp@gmail.com>
2026-08-21 18:00:00 +0000

    missing number

---
+++ Easy/268_missing_number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        res = 0
+        n = len(nums)
+
+        for i in range(n + 1):
+            res ^= i
+            if i < n: res ^= nums[i]
+        
+        return res
